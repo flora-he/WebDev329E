eb18935159a570f2c3927061858b6b045b1e9fb4
flora-he <64234132+flora-he@users.noreply.github.com>
2023-07-18 22:44:17 -0500

    Create today.php in demos folder

---
+++ demos/today.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+   <title>First PHP Demo</title>
+   <meta charset="UTF-8">
+   <meta name="description" content="ENTER DESCRIPTION HERE">
+   <meta name="author" content="ENTER NAME HERE">
+</head> 
+
+<body>
+
+<!-- laptops do not have PHP, servers do  -->
+<p>Hello! Today is 
+   <?php
+      echo date('l, F, jS, Y')
+   ?>
+</p>
+
+</body>
+</html>
