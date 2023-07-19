dcfe7153159398e0746fe4e143e22815687fd55f
flora-he <64234132+flora-he@users.noreply.github.com>
2023-07-18 22:54:13 -0500

    Create exam2/tictac.php

---
+++ exam2/tictac.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+   <title>Tic-Tac-Toe</title>
+   <meta charset="UTF-8">
+   <meta name="description" content="ENTER DESCRIPTION HERE">
+   <meta name="author" content="Flora He">
+</head> 
+
+<body>
+   <h2 align="center"> Tic-Tac-Toe </h2>
+
+    <!-- Create the table -->
+   <p>
+   <table border="border" align="center" style="width: 50%; height: 50%">
+    <tr>
+           <td align = "center"> 
+               <input type = "button" name = "unpop" 
+                      style="width:35%" /> </td>
+           <td align = "center"> <input type = "button" name = "unpop" 
+                      style="width:35%" /> </td>
+           <td align = "center"> 
+               <input type = "button" name = "unpop" 
+                      style="width:35%" /></td>
+       </tr>
+       <tr>
+            <td align = "center"> 
+               <input type = "button" name = "unpop" 
+                      style="width:35%" /></td>
+           <td align = "center"> <input type = "button" name = "unpop" 
+                      style="width:35%" /></td>
+           <td align = "center"> 
+               <input type = "button" name = "unpop" 
+                      style="width:35%" /></td>
+       </tr>
+       <tr>
+           <td align = "center"> 
+               <input type = "button" name = "unpop" 
+                      style="width:35%"/> </td>
+           <td align = "center"> <input type = "button" name = "unpop" 
+                      style="width:35%" /> </td>
+           <td align = "center"> 
+               <input type = "button" name = "unpop" 
+                      style="width:35%" /></td>
+       </tr>
+     </table>
+
+</body>
+</html>
