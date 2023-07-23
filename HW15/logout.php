0d88a291eb2c320d192fafde67ca07348d410b19
flora-he <64234132+flora-he@users.noreply.github.com>
2023-07-23 14:05:42 -0500

    Create HW15/logout.php

---
+++ HW15/logout.php
@@ -0,0 +1,16 @@
+<?php
+//logout.php
+session_start();
+session_destroy();
+?>
+<html>
+	<head>
+	<title></title>
+	</head>
+	<body>
+    <script language = "javascript" type = "text/javascript">  
+        window.alert("Thank You!");
+    </script>
+    </body>
+</html>
+	
